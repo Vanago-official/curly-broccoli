1ccf20321dade2e6b013168c3e7e198d7a6d3fd7
Vanago-official <vananovoselov824@gmail.com>
2026-06-25 18:36:27 +0300

    added procedure for adding item in order

--- querys/functions.sql
+++ querys/functions.sql
@@ -10,3 +10,21 @@ INSERT INTO orders (customer_id, order_date, total_amount)
 values (p_customer_id, current_timestamp, 0);
 END;
 $$;
+create or replace procedure add_product_to_order(
+    p_order_id int,
+    p_product_id int,
+    p_quantity int
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+if (select stock_quantity from products where product_id = p_product_id) - p_quantity >= 0 and p_quantity > 0 then
+    INSERT INTO order_items (order_id, product_id, quantity, price)
+    values (p_order_id, p_product_id, p_quantity, (select price from products where product_id = p_product_id));
+    update products
+    set stock_quantity = stock_quantity - p_quantity
+    where product_id = p_product_id;
+    else raise notice 'та ти шось нахімічив тут';
+    END if;
+END;
+$$;
